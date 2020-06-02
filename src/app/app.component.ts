f81f13670cf6a744bcfa659b553853724e699a48
zgoen <65671043+zgoen@users.noreply.github.com>
2020-06-02 11:08:43 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div><h1>{{pageTitle}}</h1>
+      <pm-products></pm-products>
+    </div>
+    `
 })
+
 export class AppComponent {
-  title = 'apm';
+  pageTitle: string = 'Acme Product Management'
 }
