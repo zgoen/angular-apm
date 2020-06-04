3a9569d544cbefb75a70a800211ca2f1becc4dac
zgoen <65671043+zgoen@users.noreply.github.com>
2020-06-03 19:54:51 -0500

    module practice

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('apm app is running!');
+    expect(page.getTitleText()).toEqual('Welcome to Angular: Getting Started!!');
   });
 
   afterEach(async () => {
